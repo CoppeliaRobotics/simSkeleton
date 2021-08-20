24dec571bd861ecd32195117de780d0792a3d5ac
Coppelia <mfreese@gmx.ch>
2021-08-20 09:36:59 +0200

    simplified a bit

--- simExtPluginSkeleton.cpp
+++ simExtPluginSkeleton.cpp
@@ -138,45 +138,15 @@ SIM_DLLEXPORT void simEnd()
 // This is the plugin messaging routine (i.e. CoppelisSim calls this function very often, with various messages):
 SIM_DLLEXPORT void* simMessage(int message,int* auxiliaryData,void* customData,int* replyData)
 { // This is called quite often. Just watch out for messages/events you want to handle
-    // Keep following 5 lines at the beginning and unchanged:
-    static bool refreshDlgFlag=true;
-    int errorModeSaved;
-    simGetIntegerParameter(sim_intparam_error_report_mode,&errorModeSaved);
-    simSetIntegerParameter(sim_intparam_error_report_mode,sim_api_errormessage_ignore);
     void* retVal=NULL;
 
-    // Here we can intercept many messages from CoppelisSim (actually callbacks). Only the most important messages are listed here.
+    // Here we can intercept many messages from CoppelisSim. Only the most important messages are listed here.
     // For a complete list of messages that you can intercept/react with, search for "sim_message_eventcallback"-type constants
     // in the CoppelisSim user manual.
 
-    if (message==sim_message_eventcallback_refreshdialogs)
-        refreshDlgFlag=true; // CoppelisSim dialogs were refreshed. Maybe a good idea to refresh this plugin's dialog too
-
-    if (message==sim_message_eventcallback_menuitemselected)
-    { // A custom menu bar entry was selected..
-        // here you could make a plugin's main dialog visible/invisible
-    }
-
     if (message==sim_message_eventcallback_instancepass)
     {   // This message is sent each time the scene was rendered (well, shortly after) (very often)
-        // It is important to always correctly react to events in CoppelisSim. This message is the most convenient way to do so:
 
-        int flags=auxiliaryData[0];
-        bool sceneContentChanged=((flags&(1+2+4+8+16+32+64+256))!=0); // object erased, created, model or scene loaded, und/redo called, instance switched, or object scaled since last sim_message_eventcallback_instancepass message 
-        bool instanceSwitched=((flags&64)!=0);
-
-        if (instanceSwitched)
-        {
-            // React to an instance switch here!!
-        }
-
-        if (sceneContentChanged)
-        { // we actualize plugin objects for changes in the scene
-
-            //...
-
-            refreshDlgFlag=true; // always a good idea to trigger a refresh of this plugin's dialog here
-        }
     }
 
     if (message==sim_message_eventcallback_mainscriptabouttobecalled)
@@ -193,29 +163,45 @@ SIM_DLLEXPORT void* simMessage(int message,int* auxiliaryData,void* customData,i
     { // Simulation just ended
 
     }
+    
+    if ( (message==sim_message_eventcallback_simulationinit)&&(auxiliaryData[0]==0) )
+    { // the main script's init section is about to be executed
 
-    if (message==sim_message_eventcallback_moduleopen)
-    { // A script called simOpenModule (by default the main script). Is only called during simulation.
-        if ( (customData==NULL)||(_stricmp("PluginSkeleton",(char*)customData)==0) ) // is the command also meant for this plugin?
-        {
-            // we arrive here only at the beginning of a simulation
-        }
     }
 
-    if (message==sim_message_eventcallback_modulehandle)
-    { // A script called simHandleModule (by default the main script). Is only called during simulation.
-        if ( (customData==NULL)||(_stricmp("PluginSkeleton",(char*)customData)==0) ) // is the command also meant for this plugin?
-        {
-            // we arrive here only while a simulation is running
-        }
+    if ( (message==sim_message_eventcallback_simulationinit)&&(auxiliaryData[0]==1) )
+    { // the main script's init section was executed
+
+    }
+    
+    if ( (message==sim_message_eventcallback_simulationactuation)&&(auxiliaryData[0]==0) )
+    { // the main script's actuation section is about to be executed
+
     }
 
-    if (message==sim_message_eventcallback_moduleclose)
-    { // A script called simCloseModule (by default the main script). Is only called during simulation.
-        if ( (customData==NULL)||(_stricmp("PluginSkeleton",(char*)customData)==0) ) // is the command also meant for this plugin?
-        {
-            // we arrive here only at the end of a simulation
-        }
+    if ( (message==sim_message_eventcallback_simulationactuation)&&(auxiliaryData[0]==1) )
+    { // the main script's actuation section was executed
+
+    }
+    
+    if ( (message==sim_message_eventcallback_simulationsensing)&&(auxiliaryData[0]==0) )
+    { // the main script's sensing section is about to be executed
+
+    }
+
+    if ( (message==sim_message_eventcallback_simulationsensing)&&(auxiliaryData[0]==1) )
+    { // the main script's sensing section was executed
+
+    }
+
+    if ( (message==sim_message_eventcallback_simulationcleanup)&&(auxiliaryData[0]==0) )
+    { // the main script's cleanup section is about to be executed
+
+    }
+
+    if ( (message==sim_message_eventcallback_simulationcleanup)&&(auxiliaryData[0]==1) )
+    { // the main script's cleanup section was executed
+
     }
 
     if (message==sim_message_eventcallback_instanceswitch)
@@ -223,26 +209,8 @@ SIM_DLLEXPORT void* simMessage(int message,int* auxiliaryData,void* customData,i
 
     }
 
-    if (message==sim_message_eventcallback_broadcast)
-    { // Here we have a plugin that is broadcasting data (the broadcaster will also receive this data!)
-
-    }
-
-    if (message==sim_message_eventcallback_scenesave)
-    { // The scene is about to be saved. If required do some processing here (e.g. add custom scene data to be serialized with the scene)
-
-    }
-
     // You can add many more messages to handle here
 
-    if ((message==sim_message_eventcallback_guipass)&&refreshDlgFlag)
-    { // handle refresh of the plugin's dialogs
-        // ...
-        refreshDlgFlag=false;
-    }
-
-    // Keep following unchanged:
-    simSetIntegerParameter(sim_intparam_error_report_mode,errorModeSaved); // restore previous settings
     return(retVal);
 }
 
