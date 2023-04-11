7af9ea5eb1aa1d84054c90abdaf7135a1fdbc3b3
Coppelia <mfreese@gmx.ch>
2023-04-11 16:54:14 +0200

    small refact + fvisibility=hidden

--- simExtPluginSkeleton.h
+++ simExtPluginSkeleton.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#ifdef _WIN32
-    #define SIM_DLLEXPORT extern "C" __declspec(dllexport)
-#else
-    #define SIM_DLLEXPORT extern "C"
-#endif
-
+#include <simLib/simExp.h>
 
 // The 3 required entry points of the CoppelisSim plugin:
 SIM_DLLEXPORT unsigned char simStart(void* reservedPointer,int reservedInt);
